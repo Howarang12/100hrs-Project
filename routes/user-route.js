34928c50bbca70eaf5f345b15fb2bbe173445838
Howard Wang <howard122696@gmail.com>
2022-09-15 20:42:40 -0700

    user registration route

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,14 +1,84 @@
 const express = require('express')
 const router = express.Router()
+//User model
+const User = require('../models/user-model')
+
+const bcrypt = require('bcryptjs')
 
 //login
 router.get('/login', (req, res) => {
   res.send('Login')
 })
 
-//login
+// register page
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+// register handler
+router.post('/register', async (req, res) => {
+  const { username, email, password, password2 } = req.body
+
+  let errors = []
+
+  // check password match
+  if(password !== password2) {
+    errors.push({msg: 'Passwords do not match'})
+  }
+
+  //check password length
+  if(password.length < 6){
+    errors.push({msg: 'Password should be at least 6 characters'})
+  }
+
+  if(errors.length > 0) {
+    res.render('register', {
+        errors,
+        username,
+        email,
+        password,
+        password2
+      }
+    )
+  } else {
+    // validation pass, add user to db
+      let user = await User.findOne({email: email})
+      if(user){
+        //User exists
+        errors.push({msg: 'Email is already registered'})
+        res.render('register', {
+          errors,
+          username,
+          email,
+          password,
+          password2
+        }
+      )
+      } else {
+        const newUser = new User({
+          username,
+          email,
+          password
+        })
+        // Hash Password
+        bcrypt.genSalt(10, (err, salt) => 
+          bcrypt.hash(newUser.password, salt, async (err, hash) => {
+            if(err) throw err
+            //set password to hashed 
+            newUser.password = hash
+            //save user
+            try{
+              await newUser.save()
+              res.redirect('/')
+            } catch (err) {
+              console.log(err)
+            }
+            
+        }))
+
+      }
+      
+  }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const connectDB = require('./config/connectDb')
+const flash = require('connect-flash')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const passport = require('passport')
@@ -8,24 +10,31 @@ require('dotenv').config()
 
 const app = express()
 
-//set view engine
+// connect mongodb
+connectDB()
+
+// set view engine
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
 
-//serve static files
+// serve static files
 app.use(express.static('public'))
 
-//connect mongodb
-// mongoose.connect(process.env.MONGO_URI, () => {
-//   console.log('connected to mongodb...')
-// })
+// bodyparser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   resave: false,
-//   saveUninitialized: false,
-//   store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
-// }))
+// Express Session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
+}))
+
+// connect flash
+app.use(flash())
+
 
 //initialize passport
 // app.use(passport.initialize())
@@ -35,4 +44,4 @@ app.use(express.static('public'))
 app.use('/', require('./routes/index'))
 app.use('/user', require('./routes/user-route'))
 
-app.listen(process.env.PORT || 3000, console.log('Server running...'))
+app.listen(3000, console.log('Server running...'))
