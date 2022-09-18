e22fc2f76d2bce2af43f952eceb4d697001788d2
Howard Wang <howard122696@gmail.com>
2022-09-17 22:13:28 -0700

    google oauth20 callback

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,8 +3,38 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
 const User = require('../models/user-model')
+require('dotenv').config()
 
-passport.use(new LocalStrategy({ usernameField: 'email '}, (email, password, done) => {
+//google strategy
+passport.use(
+  new GoogleStrategy({
+  //options for the google strat
+  clientID: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: '/auth/google/redirect',
+}, async (accessToken, refreshToken, profile, done) => {
+  //passport callback function
+
+  console.log('callback fired')
+  let newUser = await new User({
+    username: profile.displayName,
+    email: profile.emails[0].value,
+    password: ''
+  }).save()
+  console.log('new user created: ' + newUser)
+  console.log(newUser.password)
+  done(null, newUser)
+
+  })
+)
+
+
+
+
+
+// local strategy
+passport.use(
+  new LocalStrategy({ usernameField: 'email '}, (email, password, done) => {
   User.findOne({email: email})
     .then(user => {
       if(!user){
