c11537d0db7db3211955e7619c6e1f7e851b17e8
Howard Wang <howard122696@gmail.com>
2022-09-26 18:24:54 -0700

    render today's foods to profile

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,8 +1,35 @@
 const router = require('express').Router()
 const authCheck = require('../middleware/ensureAuth')
+const Day = require('../models/day-model')
+const Food = require('../models/food-model')
+const { format, startOfDay, startOfToday, endOfDay, endOfToday } = require('date-fns')
 
-router.get('/', authCheck.ensureAuth,  (req, res) => {
-  res.render('profile', {user: req.user})
+router.post('/get-day', async (req, res) => {
+  const dayValues = req.body.day.split('-')
+
+  const day = await Day.findOne({
+    userId: req.user.id, 
+    date: {
+      $gte: startOfDay(new Date(dayValues)),
+      $lt: endOfDay(new Date(dayValues))
+    }
+  })
+  console.log(day)
+  res.redirect('/profile')
+})
+
+router.get('/', authCheck.ensureAuth,  async (req, res) => {
+  const today = await Day.findOne({
+    userId: req.user.id, 
+    date: {
+      $gte: startOfToday(),
+      $lt: endOfToday()
+    }
+  })
+
+  const foods = await Food.find()
+
+  res.render('profile', {user: req.user, today, foods})
 })
 
 module.exports = router
